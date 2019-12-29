c48aeba3d14eee24cded5fc76f0b7662272d5445
Magentai <magenta.i@yandex.ru>
2019-12-29 19:28:03 +0300

    finalwithoutdataset

--- ReviewClassification/classificator/test/test.py
+++ ReviewClassification/classificator/test/test.py
@@ -1,2 +1,16 @@
+import pickle
+import keras
+import numpy as np
+
 if __name__ == "__main__":
-    inputfile = open('./testdata/input.txt')
+    with open('./input.txt', 'r', encoding='utf-8') as file:
+        text = file.readline()
+
+    with open('../model/REL/tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    sequences = np.array(tokenizer.texts_to_sequences(text))
+
+    model = keras.models.load_model("../model/REL/res/weights.h5")
+    pred = model.predict(sequences)
+    print(pred.tolist())
